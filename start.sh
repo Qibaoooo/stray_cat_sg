68649131605d46e7bcf651227e3a622ac7cf750a
Qibaoooo <luozhengiw@gmail.com>
2024-01-22 21:26:50 +0800

    update some scripts

--- start.sh
+++ start.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 npm install http-server -g
-cd build
-http-server -p 8081 &
-cd ..
 
-java -jar JMSChallenge.jar
+# kill $(ps aux | grep '[j]ava' | awk '{print $2}')
+# kill $(ps aux | grep '[h]ttp-server' | awk '{print $2}')
+http-server ./full_build/build -p 8081 &
+java -jar ./full_build/StrayCatsSG-0.0.1-SNAPSHOT.jar
 
