c045a904c11fa23de5b911807a72d8f291f95e1f
Qibaoooo <luozhengiw@gmail.com>
2024-01-25 19:29:05 +0800

    add initial DB data [be]

--- src/main/java/nus/iss/team11/StrayCatsSGApplication.java
+++ src/main/java/nus/iss/team11/StrayCatsSGApplication.java
@@ -1,11 +1,14 @@
 package nus.iss.team11;
 
+import java.time.LocalDate;
+
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import nus.iss.team11.model.Roll;
 import nus.iss.team11.repository.RollRepository;
 
 @SpringBootApplication
@@ -20,6 +23,11 @@ public class StrayCatsSGApplication {
 		return (args) -> {
 			// cleam start
 			rollRepo.deleteAll();
+			
+			Roll r1 = new Roll();
+			r1.setResult(99);
+			r1.setTimeOfRoll(null);
+			rollRepo.save(r1);
 		};
 	}
 	
