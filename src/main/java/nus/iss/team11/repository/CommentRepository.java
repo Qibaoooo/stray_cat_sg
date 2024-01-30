20c2f6611ed27f841916ce9d4262478f8b14b78b
wanglinfeng <wandawang1028@gmail.com>
2024-01-30 14:34:47 +0800

    change

--- src/main/java/nus/iss/team11/repository/CommentRepository.java
+++ src/main/java/nus/iss/team11/repository/CommentRepository.java
@@ -1,5 +1,14 @@
 package nus.iss.team11.repository;
 
-public interface CommentRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import nus.iss.team11.model.Comment;
+
+
+@Repository
+public interface CommentRepository extends JpaRepository<Comment,Integer>{
+
+	List<Comment> findByScsuser(SCSUser user);
+	List<Comment> findByCat(Cat cat);
 }
