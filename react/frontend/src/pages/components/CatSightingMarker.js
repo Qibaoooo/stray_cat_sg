b4de5718e9a338eae82249bcfd58d1212feffd12
Calvin <calvinpoh01@gmail.com>
2024-01-31 16:35:33 +0800

    Added infowindow to the marker

--- react/frontend/src/pages/components/CatSightingMarker.js
+++ react/frontend/src/pages/components/CatSightingMarker.js
@@ -1,11 +1,36 @@
-import React from "react";
+import React, {useState} from "react";
 import { Button } from "react-bootstrap";
+import "./CatSightingMarker.css"
 
-const CatSightingMarker = () => {
+const CatSightingMarker = ({text}) => {
+  const [showInfoWindow, setShowInfoWindow] = useState(false);
+  const toggleInfoWindow = () => {
+    setShowInfoWindow(!showInfoWindow);
+  };
+
+  const closeInfoWindow = () => {
+    setShowInfoWindow(false);
+  };
+
+
   return (
-    <Button size="sm" style={{backgroundColor:"transparent", border:"0px solid red"}}>
-      <h3>🐱</h3>
-    </Button>
+    <div style={{ position: 'relative' }}>
+      <Button
+        size="sm"
+        style={{ backgroundColor: "transparent", border: "0px" }}
+        onClick={toggleInfoWindow}
+      >
+        <h3>🐱</h3>
+      </Button>
+
+      {showInfoWindow && (
+        <div className="info-window">
+          <button className="close-button" onClick={closeInfoWindow}>&times;</button>
+          <h4>Cat Sighting</h4>
+          <p>{text || 'Details about the sighting...'}</p>
+        </div>
+      )}
+    </div>
   );
 };
 
