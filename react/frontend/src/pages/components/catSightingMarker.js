f21f682213ee8f38db1bdff0703276f5626a855e
Qibaoooo <luozhengiw@gmail.com>
2024-02-02 16:40:11 +0800

    update core.ignorecase config

---
+++ react/frontend/src/pages/components/catSightingMarker.js
@@ -0,0 +1,46 @@
+import React, { useState } from "react";
+import { Button } from "react-bootstrap";
+import "./catSightingMarker.css";
+
+const CatSightingMarker = ({ sighting }) => {
+  const [showInfoWindow, setShowInfoWindow] = useState(false);
+  const toggleInfoWindow = () => {
+    setShowInfoWindow(!showInfoWindow);
+  };
+
+  const closeInfoWindow = () => {
+    setShowInfoWindow(false);
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <Button
+        size="sm"
+        style={{ backgroundColor: "transparent", border: "0px" }}
+        onClick={toggleInfoWindow}
+      >
+        <h3>🐱</h3>
+      </Button>
+
+      {showInfoWindow && (
+        <div className="info-window">
+          <h4>{sighting.text}</h4>
+          <img
+            alt="cat"
+            src={sighting.imagesURLs[0]}
+            style={{ width: "150px", border: "1px solid #ddd" }}
+          />
+          <p>{sighting.cat ? `cat id ${sighting.cat}` : "unknown cat"}</p>
+          <p>
+            <a href={`/catDetails?id=${sighting.cat}`}>see details</a>
+          </p>
+          <button className="close-button" onClick={closeInfoWindow}>
+            &times;
+          </button>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CatSightingMarker;
