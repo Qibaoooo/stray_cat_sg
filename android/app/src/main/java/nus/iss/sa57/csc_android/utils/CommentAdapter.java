45364716a38030c1e3eeabe9c146d153ec0c5f8a
HMM020223 <153045845+HMMMM2002@users.noreply.github.com>
2024-02-12 00:26:18 +0800

    comment list setup

--- android/app/src/main/java/nus/iss/sa57/csc_android/utils/CommentAdapter.java
+++ android/app/src/main/java/nus/iss/sa57/csc_android/utils/CommentAdapter.java
@@ -1,12 +1,20 @@
 package nus.iss.sa57.csc_android.utils;
 
+import android.app.Activity;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Environment;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 
+import java.io.File;
 import java.util.List;
 
 import nus.iss.sa57.csc_android.R;
@@ -24,6 +32,24 @@ public class CommentAdapter extends ArrayAdapter<Object> {
 
     @Override
     public View getView(int pos, View view, @NonNull ViewGroup parent) {
+        if (view == null) {
+            LayoutInflater inflater = (LayoutInflater) context.getSystemService(Activity.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.comments_list, parent, false);
+        }
+        //ImageView avatar = view.findViewById(R.id.avatar);
+        //File externalFilesDir = context.getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        //File destFile = new File(externalFilesDir, ("img-" + commentList.get(pos).getId() + "-0"));
+        //Bitmap bitmap = BitmapFactory.decodeFile(destFile.getAbsolutePath());
+        //avatar.setImageBitmap(bitmap);
+        TextView nickname = view.findViewById(R.id.nickname);
+        nickname.setText(commentList.get(pos).getScsUser().getUsername());
+        TextView labels = view.findViewById(R.id.labels);
+        //labels.setText(commentList.get(pos).getNewlabels().get(0));
+        TextView publictime = view.findViewById(R.id.publictime);
+        publictime.setText(commentList.get(pos).getTime().substring(0,10));
+        TextView content=view.findViewById(R.id.content);
+        content.setText(commentList.get(pos).getContent());
         return view;
     }
+
 }
