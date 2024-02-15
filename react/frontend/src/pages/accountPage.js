d18b4f4a38a03b2e2436dcaaa85d39b792215d25
Qibaoooo <luozhengiw@gmail.com>
2024-02-15 14:32:01 +0800

    add owner info [fe]

--- react/frontend/src/pages/accountPage.js
+++ react/frontend/src/pages/accountPage.js
@@ -4,7 +4,10 @@ import banner from "../images/top_banner.png";
 import { RxAvatar } from "react-icons/rx";
 import { useSearchParams } from "react-router-dom";
 import { getUserById } from "./utils/api/apiUser";
-import { clearUserInfoAndRedirectToLogin } from "./utils/userinfo";
+import {
+  clearUserInfoAndRedirectToLogin,
+  getUserinfoFromLocal,
+} from "./utils/userinfo";
 import LoginLogoutButton from "./components/loginLogoutButton";
 
 const accountPageButtonClass = "m-auto my-3 border-1 bg-secondary";
@@ -14,19 +17,18 @@ const OwnerButton = ({ user }) => {
     <Button
       className={accountPageButtonClass}
       onClick={() => {
-         if (!!user.isOwner) {
-           window.location.href = "/newSighting";
-         }else{
-          window.location.href="/uploadVfcation";
-         }
-       }}
-     >
-       {!!user.isOwner ? (
-         <u>Search For Lost Cat</u>
-       ) : (
-         <u>Apply to be an owner</u>
-       )}
-  
+        if (!!user.isOwner) {
+          window.location.href = "/newSighting";
+        } else {
+          window.location.href = "/uploadVfcation";
+        }
+      }}
+    >
+      {!!user.isOwner ? (
+        <u>Search For Lost Cat</u>
+      ) : (
+        <u>Apply to be an owner</u>
+      )}
     </Button>
   );
 };
@@ -48,16 +50,13 @@ const AccountPage = () => {
   });
 
   useEffect(() => {
-    getUserById(userId)
-      .then((resp) => {
-        console.log(resp.data);     
-        SetUser({ ...user, ...resp.data });  
-    })
-      .catch((e) => {
-        clearUserInfoAndRedirectToLogin();
-      });
+    const _user = getUserinfoFromLocal();
+    if (!_user) {
+      window.location.href = "/"
+    }
+    _user.isOwner = (_user.role === "ROLE_owner")
+    SetUser(_user);
   }, []);
-  
 
   return (
     <Row className="g-0">
@@ -75,7 +74,13 @@ const AccountPage = () => {
             className="my-3 bg-primary rounded-circle"
             size={80}
           ></RxAvatar>
-          <p>hi, {user.username}</p>
+          <p>
+            <em>{user.username}</em>
+          </p>
+          <p>
+            your role is:
+            <em>{user.role}</em>
+          </p>
         </div>
         <Stack direction="horizontal">
           <OwnerButton user={user}></OwnerButton>
