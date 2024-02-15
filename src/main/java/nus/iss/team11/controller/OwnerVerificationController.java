e17b0dcf012aa297931625ac64063e7b69cbdb1d
Qibaoooo <luozhengiw@gmail.com>
2024-02-15 14:18:24 +0800

    bug fix

--- src/main/java/nus/iss/team11/controller/OwnerVerificationController.java
+++ src/main/java/nus/iss/team11/controller/OwnerVerificationController.java
@@ -9,48 +9,45 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-import nus.iss.team11.Payload.NewSCSUserRequest;
 import nus.iss.team11.Payload.NewVerificationRequest;
 import nus.iss.team11.controller.service.OwnerVerificationService;
-import nus.iss.team11.model.CatSighting;
 import nus.iss.team11.model.OwnerVerification;
-import nus.iss.team11.model.SCSUser;
 
 @Controller
 @CrossOrigin(origins = "*", maxAge = 3600)
 public class OwnerVerificationController {
 	@Autowired
 	OwnerVerificationService ownerVerificationService;
-	
+
 	@GetMapping(value = "/api/verification")
 	public ResponseEntity<String> getAllOV() {
 		List<OwnerVerification> OVs = ownerVerificationService.findAllOVs();
 		JSONArray json = new JSONArray();
-		
+
 		OVs.forEach(ov -> {
 			json.put(ov.toJSON());
 		});
-		
-		return new ResponseEntity<String> (json.toString(), HttpStatus.OK);
+
+		return new ResponseEntity<String>(json.toString(), HttpStatus.OK);
 	}
-	
-	@PostMapping(value="/api/verification")
-	public ResponseEntity<String> createOwnerVerification(@RequestBody NewVerificationRequest newVerificationRequest){
+
+	@PostMapping(value = "/api/verification")
+	public ResponseEntity<String> createOwnerVerification(@RequestBody NewVerificationRequest newVerificationRequest) {
 		OwnerVerification verificationToBeSaved = new OwnerVerification();
-		
+
 		return SaveVerificationToDB(newVerificationRequest, verificationToBeSaved);
 	}
-	
-	public ResponseEntity<String> SaveVerificationToDB(NewVerificationRequest newVerificationRequest, OwnerVerification verificationToBeSaved) {
+
+	public ResponseEntity<String> SaveVerificationToDB(NewVerificationRequest newVerificationRequest,
+			OwnerVerification verificationToBeSaved) {
 		verificationToBeSaved.setStatus("Pending");
-;		verificationToBeSaved.setImageURL((newVerificationRequest.getImageURL()));
+		verificationToBeSaved.setImageURL((newVerificationRequest.getImageURL()));
 		verificationToBeSaved.setId(newVerificationRequest.getUserId());
 		verificationToBeSaved = ownerVerificationService.saveOwnerVerification(verificationToBeSaved);
 		return new ResponseEntity<>("Saved: " + String.valueOf(verificationToBeSaved.getId()), HttpStatus.OK);
 	}
-	
+
 }
